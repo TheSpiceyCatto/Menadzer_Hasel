58216bb75811d82e22d72d7d58744efa1a82e23a
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2023-05-24 13:46:47 +0200

    Fixed CMakeList

--- cmake-build-debug/CMakeFiles/Menadzer_Hasel.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Menadzer_Hasel.dir/cmake_clean.cmake
@@ -1,6 +1,4 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Menadzer_Hasel.dir/Komendy.cpp.obj"
-  "CMakeFiles/Menadzer_Hasel.dir/Komendy.cpp.obj.d"
   "CMakeFiles/Menadzer_Hasel.dir/main.cpp.obj"
   "CMakeFiles/Menadzer_Hasel.dir/main.cpp.obj.d"
   "Menadzer_Hasel.exe"
