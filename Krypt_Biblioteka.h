44ade4f4de47d940c4d73b4c5ef60439cbb00afa
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2023-05-27 18:12:15 +0200

    Generowanie haseł

--- Krypt_Biblioteka.h
+++ Krypt_Biblioteka.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <random>
 using namespace std;
 
 #ifndef MENADZER_HASEL_KOMENDY_H
@@ -99,19 +100,30 @@ namespace Biblioteka{
                 cout << "Podaj login (zostaw puste by pominac):" << endl;
                 getline(cin, buffLogin);
 
-                cout << "Podaj haslo:" << endl;
-                getline(cin, buffHaslo);
-                while(cin.fail() || buffHaslo.empty()){
-                    cout << "Nie podano hasla, sprobuj ponownie:" << endl;
-                    getline(cin, buffHaslo);
-                }
+                do {
+                    string input;
+                    cout << "Czy wygenerowac haslo? Y/n" << endl;
+                    getline(cin, input);
+                    if (toupper(input[0]) == 'N') {
+                        cout << "Podaj haslo:" << endl;
+                        getline(cin, buffHaslo);
+                        while (cin.fail() || buffHaslo.empty()) {
+                            cout << "Nie podano hasla, sprobuj ponownie:" << endl;
+                            getline(cin, buffHaslo);
+                        }
+                    } else if (toupper(input[0]) == 'Y' || input[0] == '\0') {
+                        buffHaslo = GeneratePassword();
+                    } else {
+                        cout << "Nie rozpoznano odpowiedzi" << endl;
+                    }
+                } while (buffHaslo.empty());
 
                 cout << "Podaj strone WWW (zostaw puste by pominac):" << endl;
                 getline(cin, buffStrona);
 
-                buffKategoria = WybierzKategorie(kategorie);
+                buffKategoria = ChooseCategory(kategorie);
 
-                Haslo haslo(buffNazwa, Kryptografia::Enkrypcja(buffHaslo, key), buffKategoria);
+                Haslo haslo(buffNazwa, buffHaslo, buffKategoria);
 
                 if(!buffStrona.empty()){
                     haslo.setStrona(buffStrona);
@@ -127,7 +139,8 @@ namespace Biblioteka{
             }
         }
 
-        static string WybierzKategorie(vector<string>& kategorie){
+    //private:
+        static string ChooseCategory(vector<string>& kategorie){
             cout << "Wybierz kategorie (podaj numer):" << endl;
             for(int i = 0; i < kategorie.size(); i++){
                 cout << i+1 << ". " << kategorie[i] << endl;
@@ -139,10 +152,104 @@ namespace Biblioteka{
                 if(choice > 0 && choice <= kategorie.size()){
                     correctChoice = true;
                 } else {
-                    cout << "Nie poprawny wybor kategorii." << endl;
+                    cout << "Niepoprawny wybor kategorii." << endl;
                 }
             }
             return kategorie.at(choice-1);
+        }
+
+        static string GeneratePassword(){
+            int charNum;
+            bool upLow, special, numbers;
+            string input;
+
+            cout << "Podaj ilosc znakow:" << endl;
+            getline(cin, input);
+            while(cin.fail() || input.empty() || !isNumber(input) || (isNumber(input) && stoi(input) <= 0)){
+                cout << "Niepoprawna odpowiedz, sprobuj ponownie:" << endl;
+                getline(cin, input);
+            }
+            charNum = stoi(input);
+
+            Boolean_Question("Czy uzywac wielkich i malych liter? Y/n", upLow);
+            Boolean_Question("Czy uzywac liczb? Y/n", numbers);
+            Boolean_Question("Czy uzywac znakow specjalnych? Y/n", special);
+
+            int letterMax = upLow ? 1 : 0;
+            int choiceMin = numbers ? 1 : 2;
+            int choiceMax = special ? 3 : 2;
+            string result;
+            for (int i = 0; i < charNum; ++i) {
+                int randChoice = getRandomNumber(choiceMin, choiceMax);
+                switch (randChoice) {
+                    case 1:
+                        //liczby
+                        result += (char) getRandomNumber(48, 57);
+                        break;
+                    case 2:
+                        //litery
+                        if(getRandomNumber(0, letterMax) == 1){
+                            result += (char)getRandomNumber(65, 90);
+                        } else {
+                            result += (char) getRandomNumber(97, 122);
+                        }
+                        break;
+                    case 3:
+                        //znaki specjalne
+                        int specialChoice = getRandomNumber(1, 4);
+                        switch (specialChoice) {
+                            case 1:
+                                result += (char)getRandomNumber(33, 47);
+                                break;
+                            case 2:
+                                result += (char) getRandomNumber(58, 64);
+                                break;
+                            case 3:
+                                result += (char) getRandomNumber(91, 96);
+                                break;
+                            case 4:
+                                result += (char) getRandomNumber(123, 126);
+                                break;
+                        }
+                        break;
+                }
+            }
+            return result;
+        }
+
+        static void Boolean_Question(const string mess, bool& result){
+            bool answered = false;
+            string input;
+            do{
+                cout << mess << endl;
+                getline(cin, input);
+                if (toupper(input[0]) == 'N') {
+                    result = false;
+                    answered = true;
+                } else if (toupper(input[0]) == 'Y' || input[0] == '\0') {
+                    result = true;
+                    answered = true;
+                } else {
+                    cout << "Nie rozpoznano odpowiedzi" << endl;
+                }
+            } while (!answered);
+        }
+
+        static bool isNumber(string& str) {
+            try {
+                size_t pos = 0;
+                stoi(str, &pos);
+                return pos == str.length();
+            } catch (const exception&) {
+                return false;
+            }
+        }
+
+        static int getRandomNumber(int min, int max) {
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<int> distribution(min, max);
+            return distribution(gen);
         }
     };
 }
